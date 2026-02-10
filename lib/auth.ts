a68ffee3238360773d3751c1f18f02eed4ed8041
Pius Opoku-Fofie <pius1919@gmail.com>
2026-02-10 09:51:07 +0000

    good progress ..

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,7 @@ import {
 } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-
+import { prisma } from "./prisma";
 
 
 export const auth = betterAuth({
