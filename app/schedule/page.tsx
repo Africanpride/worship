bef8bc2b13621c1f08be61ee870197ac4c791875
Pius Opoku-Fofie <pius1919@gmail.com>
2026-02-14 00:57:59 +0000

    added pages

---
+++ app/schedule/page.tsx
@@ -0,0 +1,14 @@
+import LocomotiveScrollWrapper from "@/components/LocomotiveScrollWrapper";
+
+export default function Home() {
+  return (
+    <LocomotiveScrollWrapper>
+      <h1 className="title">Basic Usage</h1>
+      <div className="wrapper">
+        <div className="box" data-scroll data-scroll-speed="-0.2"></div>
+        <div className="box" data-scroll data-scroll-speed="0.3"></div>
+      </div>
+      <div style={{ height: "200vh" }}></div>
+    </LocomotiveScrollWrapper>
+  );
+}
